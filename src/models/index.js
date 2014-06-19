3fac40e6419e4e4428f78dafdad3a9f1403b057e
edjafarov <djkojb@gmail.com>
2014-06-19 09:23:23 +0300

    reconnects

--- src/models/index.js
+++ src/models/index.js
@@ -3,23 +3,32 @@ var redis = require('redis');
 var redisUrl = require('parse-redis-url')(redis);
 var options = redisUrl.parse(process.env.REDIS_URL);
 
-var client = redis.createClient(options.port, options.host);
+var client;
 
-client.on('error', function(err){
-  console.log("global redis error" + err);
-});
-if(options.password){
-  client.auth(options.password, function(err){
-    console.log(err);
-  })
+
+function strongConnect(){
+  client = redis.createClient(options.port, options.host);
+  client.on('error', function(err){
+    console.log("global redis error: " + err);
+    strongConnect();
+  });
+
+
+  if(options.password){
+    client.auth(options.password, function(err){
+      console.log(err);
+    })
+  }
+
+  if(!process.env.REDIS_URL){
+  client.select(options.database || nconf.get('redis:db'), function(){
+  });
+  }
+  if(module.exports){
+    module.exports.db = client;
+  }
 }
-
-if(!process.env.REDIS_URL){
-client.select(options.database || nconf.get('redis:db'), function(){
-});
-}
-
-
+strongConnect()
 
 module.exports = {
   user : require('./user.js')(client),
