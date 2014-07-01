99c253fda084128e68b68f02e426617295c72fd1
Eldar Djafarov <djkojb@gmail.com>
2014-07-01 18:55:57 +0000

    serve packages

--- client/reacts/Download.jsx
+++ client/reacts/Download.jsx
@@ -3,15 +3,19 @@ module.exports = React.createClass({
   render: function(){
     return <div className="wrapper">
       <ul className="chat">
+        <li>
+	Mac: <a href="/JSChat/mac/JSChat.dmg">JSChat.dmg</a>
+        </li>
+        <li>
+	Win: <a href="/JSChat/win/JSChat.zip">JSChat.zip</a>
+        </li>
+        <li>
+	Linux32: <a href="/JSChat/linux32/JSChat.tar.gz">JSChat.tar.gz</a>
+        </li>
 	<li>
-	Mac: <a href="/desktop/releases/JSChat/mac/JSChat.dmg">JSChat.dmg</a>
+	Linux64: <a href="/JSChat/linux64/JSChat.tar.gz">JSChat.tar.gz</a>
 	</li>
-	<li>
-	Win: <a href="/desktop/releases/JSChat/win/JSChat.zip">JSChat.zip</a>
-	</li>
-	<li>
-	Linux: <a href="/desktop/releases/JSChat/mac/JSChat.tar.gz">JSChat.tar.gz</a>
-	</li>
+
       </ul>
     </div>
   }
